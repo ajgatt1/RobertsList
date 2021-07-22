81e8cb48c2d670327b522915abc8d8263b6eb737
aj <1107519@rgu.ac.uk>
2021-07-23 00:33:56 +0100

    Client Styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
+
 
 import recruitment from './images/recruitment.jpeg';
 
 const App = () => {
+    const classes = useStyles();
     return (
         <Container maxidth="lg">
-            <AppBar position="static" color ="inherit">
-                <Typography variant="h2" align="center">Robert's List</Typography>
+            <AppBar className={classes.appBar} position="static" color ="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Robert's List</Typography>
             </AppBar>
             <Grow in>
                 <Container>
